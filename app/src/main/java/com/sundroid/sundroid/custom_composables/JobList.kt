0aa280a4e168676066f6f482dad51416877b227b
adoyi8 <adegbesundayadoyi@gmail.com>
2023-03-15 02:25:59 +0100

    Started properly implementing authentication logic

--- app/src/main/java/com/sundroid/sundroid/custom_composables/JobList.kt
+++ app/src/main/java/com/sundroid/sundroid/custom_composables/JobList.kt
@@ -8,17 +8,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.sundroid.sundroid.R
 import com.sundroid.sundroid.models.Job
-
-
 
 
 @Preview
@@ -63,12 +64,41 @@ fun MyCard(job: Job) {
                         style = MaterialTheme.typography.bodySmall
                     )
                 }
+                Spacer(modifier = Modifier.height(20.dp))
+                Row() {
+                    StatusView(R.drawable.ic_sync_status, job.syncStatus)
+                    StatusView(R.drawable.ic_payment_status, job.paymentStatus)
+                    StatusView(R.drawable.ic_done, job.doneStatus)
+                    StatusView(R.drawable.ic_delivered, job.deliveredStatus)
+
+                }
             }
-            Image(painter = painterResource(id = R.drawable.ic_jobs2), contentDescription = "Hello", colorFilter =  ColorFilter.tint(MaterialTheme.colorScheme.primary),modifier = Modifier
-                .padding(16.dp).width(90.dp))
+
+            Column() {
+                val modifier = Modifier
+                    .padding(0.dp, 16.dp, 16.dp, 0.dp)
+                    .width(100.dp)
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_jobs2),
+                    contentDescription = "Hello",
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                    modifier = modifier,
+                    alignment = Alignment.TopEnd
+
+                )
+                formatCurrency( job.amount.toString())?.let { Text(text = it, modifier= modifier.width(100.dp), textAlign = TextAlign.Center, style = MaterialTheme.typography.titleMedium) }
+            }
         }
 
     }
+}
+
+@Composable
+fun StatusView(icon : Int, state: Boolean){
+    Image(painter = painterResource(id = icon), contentDescription = "Hello", colorFilter = getColorFilter(state) ,modifier = Modifier
+        .padding(5.dp)
+        .width(45.dp))
 }
 @Preview
 @Composable
@@ -79,4 +109,18 @@ class MyListProvider : PreviewParameterProvider<ArrayList<Job>> {
     override val values: Sequence<ArrayList<Job>> = sequenceOf(
         Job.getSampleJobs()
     )
+}
+fun getColorFilter(state: Boolean) : ColorFilter{
+  return  if(state) ColorFilter.tint(Color.Green) else  ColorFilter.tint(Color.Gray)
+}
+fun formatCurrency(amount: String): String? {
+    var amount = amount
+    amount = try {
+        // The comma in the format specifier does the trick
+        "₦" + String.format("%,.2f", amount.toFloat())
+    } catch (e: NumberFormatException) {
+        e.printStackTrace()
+        return amount
+    }
+    return amount
 }
--- app/src/main/java/com/sundroid/sundroid/models/Job.kt
+++ app/src/main/java/com/sundroid/sundroid/models/Job.kt
@@ -1,7 +1,5 @@
 package com.sundroid.sundroid.models
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import java.time.LocalDate
 
 
@@ -20,15 +18,15 @@ data class Job(
     val timeDelivered: LocalDate?,
     val description : String?,
     val jobId: Int,
-    val paymentStatus: Boolean?,
-    val syncStatus: Boolean?,
-    val doneStatus: Boolean?,
-    val deliveredStatus: Boolean?,
+    val paymentStatus: Boolean = false,
+    val syncStatus: Boolean = false,
+    val doneStatus: Boolean = false,
+    val deliveredStatus: Boolean = false,
     val amount: Long?,
 
 ){
     companion object {
-        @RequiresApi(Build.VERSION_CODES.O)
+
         fun getSampleJobs(): ArrayList<Job> {
             val jobs = arrayListOf<Job>()
             var job = Job(
@@ -45,6 +43,19 @@ data class Job(
                 amount = 2000
             )
             jobs.add(job)
+             job = Job(
+                customerName = "Daniel Adegbe",
+                customerEmail = "adegbesundayadoyi@gmail.com",
+                timeReceived = LocalDate.now(),
+                timeDelivered = LocalDate.now(),
+                description = "Daniel Washing Only",
+                jobId = 2,
+                paymentStatus = true,
+                syncStatus = true,
+                doneStatus = false,
+                deliveredStatus = false,
+                amount = 2000
+            )
             return jobs
         }
     }
