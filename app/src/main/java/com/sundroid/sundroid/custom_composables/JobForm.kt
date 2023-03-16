606af5ddd83aff670e07c371fc0584abc728d51b
adoyi8 <adegbesundayadoyi@gmail.com>
2023-03-16 20:22:23 +0100

    Started properly implementing authentication logic

--- app/src/main/java/com/sundroid/sundroid/custom_composables/JobForm.kt
+++ app/src/main/java/com/sundroid/sundroid/custom_composables/JobForm.kt
@@ -1,52 +1,52 @@
 package com.sundroid.sundroid.custom_composables
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.*
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.*
 import androidx.compose.ui.unit.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MaterialDesignExample() {
-    MaterialTheme {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text("Welcome to Material Design 3!", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(16.dp))
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Enter your name") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {},
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                contentPadding = PaddingValues(vertical = 12.dp, horizontal = 16.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text("Submit", color = Color.White, fontWeight = FontWeight.Bold)
-                    Icon(
-                        imageVector = Icons.Default.Favorite,
-                        contentDescription = "Favorite",
-                        tint = Color.White,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-            }
+fun JobForm(
+
+
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Gray,
+            disabledBorderColor = Color.Gray,
+            disabledTextColor = Color.Black
+        ),
+        placeholder = {
+            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
         }
-    }
+    )
 }
