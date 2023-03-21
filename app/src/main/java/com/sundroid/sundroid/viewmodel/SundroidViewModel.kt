b22b7fed39720d720cd5ffddb5131b7549564929
adoyi8 <adegbesundayadoyi@gmail.com>
2023-03-21 18:33:25 +0100

    Added Bottom Sheet

--- app/src/main/java/com/sundroid/sundroid/viewmodel/SundroidViewModel.kt
+++ app/src/main/java/com/sundroid/sundroid/viewmodel/SundroidViewModel.kt
@@ -64,7 +64,7 @@ class SundroidViewModel(application: Application) : AndroidViewModel(application
         userRepository.deleteAllUsers()
         println("delete all users called in sundroid viewmodel")
 
-        isLoading= true
+        isLoading= false
     }
     fun getCurrentUser() = viewModelScope.launch {
         users.collect {
